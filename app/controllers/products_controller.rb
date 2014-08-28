6c42a4cf5271c94ce86a5ba28f57f6ceb6318870
cburch <cody.burchell@gmail.com>
2014-08-27 22:31:02 -0500

    Add products. Posts index shows preview of post.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,58 @@
-class ProductsController
+class ProductsController < ApplicationController
+	def index
+		@products = Product.all
+	end
 
+	def show
+		@product = Product.find(params[:id])
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+
+		if @product.save
+			flash[:success] = "New product created"
+			redirect_to products_path
+		else
+			flash[:error] = "Product could not be saved"
+			render action: :new
+		end
+	end
+
+	def edit
+		@product = Product.find(params[:id])
+	end
+
+	def update
+		@product = Product.find(params[:id])
+
+		if @product.update_attributes(product_params)
+			flash[:success] = "Product updated"
+			redirect_to products_path
+		else
+			flash[:error] = "Product could not be updated"
+			render action: :edit
+		end
+	end
+
+	def destroy
+		@product = Product.find(params[:id])
+
+		if @product.destroy
+			flash[:success] = "Product deleted"
+		else
+			flash[:error] = "Product could not be deleted"
+		end
+
+		redirect_to products_path
+	end
+
+	private
+		def product_params
+			params.require(:product).permit(:name, :description, :price, :photo)
+		end
 end
