d1b662ca4d79418d95d316bbd7f4a205fc07fbb4
nullmoose <cody.burchell@gmail.com>
2014-09-19 19:46:36 -0500

    Make products a piggybak sellable item

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
 	has_attached_file :photo, :styles => { :large => "600x600>", :medium => "250x250>", :thumb => "100x100>" }
 	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+	acts_as_sellable
 end
