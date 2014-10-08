460d3503796825e7c36ae39471ba32c54b5289ea
Cody Burchell <cody.burchell@gmail.com>
2014-10-07 22:06:51 -0500

    Add piggybak and devise

--- db/migrate/20140828015456_create_products.rb
+++ db/migrate/20140828015456_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string :name
       t.text :description
-      t.decimal :price
+      t.decimal :price, precision: 16, scale: 2
 
       t.timestamps
     end
